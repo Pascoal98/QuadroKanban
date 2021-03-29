43ee0209e37a5a464b08890614484bcabf2cd1a3
Bruno Pascoal <brunompp98@gmail.com>
2021-03-29 20:53:29 +0100

    Adiçao do .c e .h de task que vai funcionar como base

---
+++ src/task.h
@@ -0,0 +1,11 @@
+typedef struct tarefa{
+    int fase = 0;  // 0 = ToDo, 1 = Doing, 2 = Done;
+    int id;
+    int prioridade;
+    char descricao[50];
+    char pessoa[20];
+    int dataCriacao;
+    int dataLimite;
+    int dataConclusao;
+    struct Tarefa* next;
+}TAREFA;
