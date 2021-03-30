9a8fa26128396718a6f53012ac42609828ac0d73
Bruno Pascoal <brunompp98@gmail.com>
2021-03-30 20:40:51 +0100

    adiçao doing task e next na struct

---
+++ src/doing.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "task.h"
+#include "doing.h"
+
+DOING *addDoingTask(TAREFA *a) {
+
+    DOING *b = (DOING*)malloc(sizeof(DOING));
+    if(b == NULL)
+        fprintf(stderr, "Can't allocate memory for the task\n");
+    
+    *b->doingID = a->id;
+    *b->nomePessoa = a->pessoa;
+    *b->dataLimite = a->dataLimite;
+    *b->nextDoing = NULL;
+    return *b;
+}
+
--- src/doing.h
+++ src/doing.h
@@ -5,4 +5,5 @@ typedef struct doing{
     TAREFA *doingID; //ID da tarefa overall para nao perder de vista
     TAREFA *nomePessoa[20];
     TAREFA *dataLimite;
+    struct DOING *nextDoing;
 }DOING;
